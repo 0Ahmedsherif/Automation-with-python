6595ce13c07df9e968622d0b1fdcce54ae133e25
0Ahmedsherif <89992111+0Ahmedsherif@users.noreply.github.com>
2022-03-20 17:23:41 +0200

    Update eks-status_check.py

--- eks-status_check.py
+++ eks-status_check.py
@@ -7,12 +7,9 @@ for cluster in clusters:
     response = client.describe_cluster(
         name=cluster
     )
-    cluster_info = response['cluster']  # var to avoid repetitive code
-    # cluster_status = response['cluster']['status']  # not optimum way to write the code.
+    cluster_info = response['cluster'
     cluster_status = cluster_info['status']
     cluster_endpoint = cluster_info['endpoint']
     cluster_version = cluster_info['version']
     print(f"Cluster name: {cluster}\nStatus: {cluster_status}\nEndpoint: {cluster_endpoint}\n"
           f"Version: {cluster_version}")
-
-
